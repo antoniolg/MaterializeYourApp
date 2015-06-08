26db614215a4afbe337eadafdd360c31990f3333
Antonio Leiva <antonioleivag@gmail.com>
2015-06-08 23:02:21 +0200

    Fix license year

--- app/src/main/java/com/antonioleiva/materializeyourapp/WindowCompatUtils.java
+++ app/src/main/java/com/antonioleiva/materializeyourapp/WindowCompatUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) ${YEAR} Antonio Leiva
+ * Copyright (C) 2015 Antonio Leiva
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
